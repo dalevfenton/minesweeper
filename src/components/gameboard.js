4fa9d2fcc2e66a3ccb78b5c4aaf324b18c31f718
Dale Fenton <fentondale@gmail.com>
2016-11-17 22:19:33 -0500

    mine counting function working properly

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -38,7 +38,7 @@ class GameBoard extends Component {
     // cell 5 = index + 1
     // cell 6 = index + width - 1
     // cell 7 = index + width
-    // cell 8 = index _ width + 1
+    // cell 8 = index + width + 1
     const width = this.props.data.width;
     const height = this.props.data.height;
     const functions = [
@@ -48,16 +48,30 @@ class GameBoard extends Component {
       function(x){ return x-1},
       function(x){ return x+1},
       function(x){ return x+width-1},
-      function(x){ return x+width-1},
-      function(x){ return x+width-1},
+      function(x){ return x+width},
+      function(x){ return x+width+1},
     ]
 
     cellArray.forEach(function(cell, index, array){
       let mineCount = 0;
-      functions.forEach(function(func){
+      functions.forEach(function(func, funcIndex){
+        const leftCol = index%width === 0 ? true : false;
+        const leftExclude = [0,3,5].indexOf(funcIndex) > -1 ? true : false;
+        const rightCol = (index+1)%width === 0 ? true : false;
+        const rightExclude = [2,4,7].indexOf(funcIndex) > -1 ? true : false;
+        //if we're on the edge of the board, don't look for mines
+        //that are actually on the other side
+        if( (leftCol && leftExclude) || (rightCol && rightExclude) ){
+          return;
+        }
+
+        //get the indices of the cells around the cell we're checking
         const value = func(index);
+        //check that the index is a valid cell
+        //invalid cells occur when the cell we're checking is
+        //along the edge of the game board
         if( value >= 0 && value <= (width*height)-1){
-          console.log(array[value]);
+          //if we've gotten this far and it's a mine then we add it to the count
           if(array[value].isMine){
             mineCount += 1;
           }
